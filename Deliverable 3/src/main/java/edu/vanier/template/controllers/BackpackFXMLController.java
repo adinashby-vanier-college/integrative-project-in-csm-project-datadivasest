f399cbfbeb9d2b3b9e25635edce9ba997a9c7815
sofiamartinezzelaya <6254086@edu.vaniercollege.qc.ca>
2025-03-30 05:59:51 -0400

    extra images loaded, fixed BackpackFXML, backpack controller add method

--- Deliverable 3/src/main/java/edu/vanier/template/controllers/BackpackFXMLController.java
+++ Deliverable 3/src/main/java/edu/vanier/template/controllers/BackpackFXMLController.java
@@ -1,9 +1,15 @@
 package edu.vanier.template.controllers;
 
+import edu.vanier.template.models.Sprite;
 import edu.vanier.template.ui.MainApp;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import org.kordamp.ikonli.fontawesome5.FontAwesomeRegular;
 import org.kordamp.ikonli.javafx.FontIcon;
@@ -21,9 +27,12 @@ public class BackpackFXMLController {
     @FXML
     Button exitBtn;
     @FXML
-    Pane itemsGridPane;
+    GridPane itemsGridPane;
     @FXML
-    Pane scrollPaneBackpack;
+    ScrollPane scrollPaneBackpack;
+
+    private int numRows = 0;
+    private int currentColumn = 0;
 
     /**
      * actions performed with the backpack
@@ -36,6 +45,24 @@ public class BackpackFXMLController {
         doneBtn.setOnAction(this::loadLastScene);
         exitBtn.setOnAction(this::loadLastScene);
     }
+
+    public void addObject(Sprite sprite) {
+        Image objectImage = sprite.getImage();
+        if(sprite.getType().equals("electron") || sprite.getType().equals("proton") || sprite.getType().equals("atom")
+                || sprite.getType().equals("power up") || sprite.getType().equals("coin")) {
+            ImageView objectImageView = new ImageView(objectImage);
+
+            itemsGridPane.add(objectImageView, currentColumn, numRows);
+            currentColumn++;
+            logger.info("Adding object: " + sprite.getType() + " to backpack");
+
+            if (currentColumn == 2) {
+                currentColumn = 0;
+                numRows++;
+            }
+        }
+    }
+
     //TODO: find a way to know what the last scene was in order to go back to it -->Hashmap?
     //TODO: find a way show whatever was selected from the backpack and show it in the right area of the
     // current scenes pane, and delete it from the backpack's data and inventory (both graphics and data)
