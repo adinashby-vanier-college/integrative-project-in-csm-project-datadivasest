6caf13f88f5353a713350b32484ee2cca494088d
Tabasuum Chowdhury <150824316+tabasuumchowdhury@users.noreply.github.com>
2025-03-30 00:59:07 -0400

    some changes main menu

--- Deliverable 3/src/main/java/edu/vanier/template/controllers/MainMenuFXMLController.java
+++ Deliverable 3/src/main/java/edu/vanier/template/controllers/MainMenuFXMLController.java
@@ -1,10 +1,14 @@
 package edu.vanier.template.controllers;
 
 import com.sun.tools.javac.Main;
+import edu.vanier.template.ui.BaseWindow;
 import edu.vanier.template.ui.MainMenu;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,6 +30,10 @@ public class MainMenuFXMLController {
     Button createAccountBtn;
     @FXML
     Button settingsBtn;
+    @FXML
+    BorderPane borderPane;
+    @FXML
+    ImageView titleImgView;
 
     /**
      * initializes the scene
@@ -38,6 +46,28 @@ public class MainMenuFXMLController {
         loginBtn.setOnAction(this::loadLoginScene);
         createAccountBtn.setOnAction(this::loadCreateAccountScene);
         settingsBtn.setOnAction(this::loadSettingsScene);
+
+        //Sets background
+        Image backgroundImg = new Image(MainAppFXMLController.class.
+                getResource("/images/Files/png/BG.png").toString());
+        BackgroundSize bSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false);
+
+        borderPane.setBackground(new Background(new BackgroundImage(backgroundImg,
+                BackgroundRepeat.REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.CENTER,
+                bSize)));
+
+        //Adds title
+        Image titleImg = new Image(MainAppFXMLController.class.
+                getResource("/images/title.png").toString());
+        titleImgView.setImage(titleImg);
+        titleImgView.setPreserveRatio(true);
+        titleImgView.setFitWidth(BaseWindow.sceneWidth * 0.5);
+        titleImgView.setFitHeight(BaseWindow.sceneHeight * 0.5);
+
+
+
     }
 
     /**
